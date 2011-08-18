075ee046646cf35174f87ba93b50dc1d1d021ac2
Felipe Nesello <fanesello@inf.ufrgs.br>
2011-08-18 13:54:59 +0000

    Colocando link no item do menu.
    git-svn-id: https://svn-redes.inf.ufrgs.br/hyman/trunks/meican@667 f8bb852b-7d42-42d5-be0b-027218e41f69

--- apps/init/data/menu_view.php
+++ apps/init/data/menu_view.php
@@ -19,16 +19,18 @@ class MenuView {
 
             $menu->item[$ind] = new Item();
             $menu->item[$ind]->name = (string) $i->name;
-
+            $menu->item[$ind]->link = ($i->link) ? (string) $i->link : NULL;
 
             $ind2 = 0;
 
-            foreach ($i->subItem as $s) {
-                $menu->item[$ind]->subItem[$ind2] = new SubItem();
-                $menu->item[$ind]->subItem[$ind2]->name = (string) $s->name;
-                $menu->item[$ind]->subItem[$ind2]->link = (string) $s->link;
-                $menu->item[$ind]->subItem[$ind2]->right = (string) $s->right;
-                $ind2++;
+            if ($i->subItem) {
+                foreach ($i->subItem as $s) {
+                    $menu->item[$ind]->subItem[$ind2] = new SubItem();
+                    $menu->item[$ind]->subItem[$ind2]->name = (string) $s->name;
+                    $menu->item[$ind]->subItem[$ind2]->link = (string) $s->link;
+                    $menu->item[$ind]->subItem[$ind2]->right = ($s->right) ? (string) $s->right : NULL;
+                    $ind2++;
+                }
             }
             $ind++;
         }
@@ -45,29 +47,33 @@ class MenuView {
 
             $temp = new Item();
             $temp->name = $i->name;
+            $temp->link = $i->link;
 
-            $ableTo = false;
-            $ind2 = 0;
+            if ($i->subItem) {
+                $ableTo = false;
+                $ind2 = 0;
+                foreach ($i->subItem as $s) {
 
-            foreach ($i->subItem as $s) {
+                    $ok = TRUE;
+                    if ($s->right) {
+//                    $right = explode('|',$s->right);
+//                    $ok = FALSE;
+//                    foreach ($right as $r)
+//                        $ok |= AuthSystem::userAbleToAccess($r);
+                    }
 
-                if ($s->right){
-                    $right = explode('|',$s->right);
-                    $ok = FALSE;
-                    foreach ($right as $r)
-                        $ok |= AuthSystem::userAbleToAccess($r);
+                    if (!($s->right) || $ok) {
 
+                        $temp->subItem[$ind2] = new SubItem();
+                        $temp->subItem[$ind2]->name = $s->name;
+                        $temp->subItem[$ind2]->link = $s->link;
+
+                        $ableTo = TRUE;
+                        $ind2++;
+                    }
                 }
-
-                if (!($s->right) || $ok){
-
-                    $temp->subItem[$ind2] = new SubItem();
-                    $temp->subItem[$ind2]->name = $s->name;
-                    $temp->subItem[$ind2]->link = $s->link;
-
-                    $ableTo = TRUE;
-                    $ind2++;
-                }
+            } else {
+                $ableTo = TRUE;
             }
 
             if ($ableTo) {
@@ -83,6 +89,7 @@ class Item {
 
     public $subItem = array();
     public $name;
+    public $link;
 
 }
 
--- apps/init/views/menu.php
+++ apps/init/views/menu.php
@@ -1,12 +1,17 @@
 <?php $menu = $this->passedArgs; ?>
 
-<?php foreach($menu->item as $i): ?>
+<?php foreach ($menu->item as $i): ?>
 
-<div class="topItem">
-<?php echo _($i->name); ?>
-            <?php foreach($i->subItem as $s): ?>
-                <div class="subItem"><a href="<?= $s->link ?>" target="main"><?php echo _($s->name); ?></a></div>
-            <?php endforeach; ?>
-</div>
+    <div class="topItem">
+        <?php if ($i->link): ?>
+            <a href="<?php echo $i->link; ?>" target="main"><?php echo _($i->name); ?></a>
+        <?php else: ?>
+            <?php echo _($i->name); ?>
+        <?php endif; ?>
+    </div>
+    
+    <?php foreach ($i->subItem as $s): ?>
+        <div class="subItem"><a href="<?php echo $s->link ?>" target="main"><?php echo _($s->name); ?></a></div>
+    <?php endforeach; ?>
 
 <?php endforeach; ?>
