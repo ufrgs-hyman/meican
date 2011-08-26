810f81ba2011b3967a6509f6373db6bfb0e7ce07
Pietro Biasuz <pfbiasuz@inf.ufrgs.br>
2011-08-26 22:04:39 +0000

    acl
    git-svn-id: https://svn-redes.inf.ufrgs.br/hyman/trunks/meican@701 f8bb852b-7d42-42d5-be0b-027218e41f69

--- db/structure/aros_acos.sql
+++ db/structure/aros_acos.sql
@@ -36,7 +36,4 @@ CREATE TABLE IF NOT EXISTS `aros_acos` (
   `update` enum('allow','deny') DEFAULT NULL,
   `delete` enum('allow','deny') DEFAULT NULL,
   PRIMARY KEY (`perm_id`),
-  UNIQUE KEY `aro_id` (`aro_id`,`aco_id`),
-  KEY `aro_id_2` (`aro_id`),
-  KEY `aco_id` (`aco_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
