b879df9fefe3ea11e7ad4e4d912ac231bd9df94d
Luís Armando Bianchin <labianchin@inf.ufrgs.br>
2011-09-21 16:16:59 +0000

    Continuação da implementação do dispatcher.
    git-svn-id: https://svn-redes.inf.ufrgs.br/hyman/trunks/meican@807 f8bb852b-7d42-42d5-be0b-027218e41f69

--- libs/dispatcher.php
+++ libs/dispatcher.php
@@ -2,62 +2,77 @@
 
 class Dispatcher {
 
-	function dispatch(){
-		if (!$this->login())
-			return ;
-		if (array_key_exists('url', $_GET))
-			$url = Common::GET('url');
-		else
-			$url = '';
-		$router = new Router(array());
-		extract($router->parse($url));
-		if (!empty($app)){
-			Language::setLang($app);
-			$app = Framework::loadApp($app);
-	
-			if (!empty($controller)) {
-				$controller = $app->loadController($controller);
-				if (!empty($action) && method_exists($controller, $action)){
-				    $controller->$action(Controller::getParam($params));
-				} else {
-					$action = $controller->getDefaultAction();
-					$controller->$action();
-				}
-			} else {
-				$controller = $app->loadController($app->getDefaultController());
+    public function __construct($defaults = array()) {
+        $this->defaults = array_merge(array('app' => '', 'controller' => '', 'action' => '', 'params' => array()));
+    }
 
-				$action = $controller->getDefaultAction();
-				$controller->$action();
-			}
-		} else {
-				$appClass = Framework::getMainApp();
-				$app = Framework::loadApp($appClass);
+    function dispatch() {
+        if (!$this->login())
+            return;
+        if (array_key_exists('url', $_GET))
+            $url = Common::GET('url');
+        else
+            $url = '';
+        extract($this->parse($url));
+        if (!empty($app)) {
+            Language::setLang($app);
+            $app = Framework::loadApp($app);
 
-				Language::setLang($appClass);
+            if (!empty($controller)) {
+                $controller = $app->loadController($controller);
+                if (!empty($action) && method_exists($controller, $action)) {
+                    $controller->$action(Controller::getParam($params));
+                } else {
+                    $action = $controller->getDefaultAction();
+                    $controller->$action();
+                }
+            } else {
+                $controller = $app->loadController($app->getDefaultController());
 
-				$controllerClass = $app->getDefaultController();
-				$controller = $app->loadController($controllerClass);
+                $action = $controller->getDefaultAction();
+                $controller->$action();
+            }
+        } else {
+            $appClass = Framework::getMainApp();
+            $app = Framework::loadApp($appClass);
 
-				$action = $controller->getDefaultAction();
-				$controller->$action();
-		}
-	}
+            Language::setLang($appClass);
 
-	function login(){
-		if (AuthSystem::userTryToLogin() || AuthSystem::isUserLoggedIn()){
-			return true;
-		} else {
-			/*$appClass = Common::GET('app');
-            $controllerClass = Common::GET('controller');
-            if (($appClass == "init") && ($controllerClass == "gui")) {
-                // user has expired the session and is trying to reload the gui - refresh or F5
-                // redirect to login
-                header('Location: index.php?message=Session Expired');
-                
-            } else header('HTTP/1.1 402 Timeout');*/
-			header('Location: index.php?message=Session Expired');
-			return false;
-		}
-	}
+            $controllerClass = $app->getDefaultController();
+            $controller = $app->loadController($controllerClass);
+
+            $action = $controller->getDefaultAction();
+            $controller->$action();
+        }
+    }
+
+    function login() {
+        if (AuthSystem::userTryToLogin() || AuthSystem::isUserLoggedIn()) {
+            return true;
+        } else {
+            /* $appClass = Common::GET('app');
+              $controllerClass = Common::GET('controller');
+              if (($appClass == "init") && ($controllerClass == "gui")) {
+              // user has expired the session and is trying to reload the gui - refresh or F5
+              // redirect to login
+              header('Location: index.php?message=Session Expired');
+
+              } else header('HTTP/1.1 402 Timeout'); */
+            header('Location: index.php?message=Session Expired');
+            return false;
+        }
+    }
+
+    public function parse($url) {
+        $val = explode('/', $url);
+        if (count($val) >= 3)
+            $route = array('app' => $val[0], 'controller' => $val[1], 'action' => $val[2], 'params' => array_slice($val, 3));
+        else if (count($val) == 2)
+            $route = array('app' => $val[0], 'controller' => $val[1]);
+        else if (count($val) == 1)
+            $route = array('app' => $val[0]);
+        $route = array_merge($this->defaults, $route);
+        return $route;
+    }
 
 }
