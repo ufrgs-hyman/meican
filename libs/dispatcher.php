b940f9ebba95c43ee655dfc683f28f5c282acc3e
Luís Armando Bianchin <labianchin@inf.ufrgs.br>
2011-09-21 16:16:59 +0000

    Mantendo urls legadas
    git-svn-id: https://svn-redes.inf.ufrgs.br/hyman/trunks/meican@808 f8bb852b-7d42-42d5-be0b-027218e41f69

--- libs/dispatcher.php
+++ libs/dispatcher.php
@@ -12,7 +12,9 @@ class Dispatcher {
         if (array_key_exists('url', $_GET))
             $url = Common::GET('url');
         else
-            $url = '';
+            $url = null;
+		if (empty($url))
+			$this->legacyDispatch();
         extract($this->parse($url));
         if (!empty($app)) {
             Language::setLang($app);
@@ -74,5 +76,78 @@ class Dispatcher {
         $route = array_merge($this->defaults, $route);
         return $route;
     }
+
+	function legacyDispatch(){
+		if (array_key_exists('app', $_GET) && array_key_exists('services', $_GET)) {
+			   $appClass = Common::GET('app');
+			   $app = Framework::loadApp($appClass);
+			   $controller = $app->loadController('ws');
+		} else {
+
+			if (AuthSystem::userTryToLogin() || AuthSystem::isUserLoggedIn()) {
+
+				if (array_key_exists('app', $_GET)) {
+				    $appClass = Common::GET('app');
+				    $app = Framework::loadApp($appClass);
+				} else $app = FALSE;
+
+
+				if (!$app) {
+				    $appClass = Framework::getMainApp();
+				    $app = Framework::loadApp($appClass);
+
+				    Language::setLang($appClass);
+
+				    $controllerClass = $app->getDefaultController();
+				    $controller = $app->loadController($controllerClass);
+
+				    $action = $controller->getDefaultAction();
+				    $controller->$action();
+				} else {
+
+				    Language::setLang($appClass);
+
+				    if (array_key_exists('controller', $_GET)) {
+				        $controllerClass = $_GET['controller'];
+				        $controller = $app->loadController($controllerClass);
+				    } else
+				        $controller = FALSE;
+
+				    if (!$controller) {
+				        $controllerClass = $app->getDefaultController();
+				        $controller = $app->loadController($controllerClass);
+
+				        $action = $controller->getDefaultAction();
+				        $controller->$action();
+				    } else {
+
+				            $action = Common::GET('action');
+				            if ($action && method_exists($controller, $action)){
+
+				                $param = Controller::getParam(Common::GET('param'));
+				                $controller->$action($param);
+				            } else {
+
+				            $action = $controller->getDefaultAction();
+				            $controller->$action();
+
+				            }
+
+				        }
+				    }
+				} else {
+				    $appClass = Common::GET('app');
+				    $controllerClass = Common::GET('controller');
+				    if (($appClass == "init") && ($controllerClass == "gui")) {
+				        // user has expired the session and is trying to reload the gui - refresh or F5
+				        // redirect to login
+				        header('Location: index.php?message=Session Expired');
+				        
+				    } else header('HTTP/1.1 402 Timeout');
+				}
+
+			}
+
+	}
 
 }
