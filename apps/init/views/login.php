b03667ecb5b33da9489b1235d2bdd4d334bcaf84
Luís Armando Bianchin <labianchin@inf.ufrgs.br>
2011-12-08 19:27:21 +0000

    Correção da tela de login.
    git-svn-id: https://svn-redes.inf.ufrgs.br/hyman/trunks/meican@914 f8bb852b-7d42-42d5-be0b-027218e41f69

--- apps/init/views/login.php
+++ apps/init/views/login.php
@@ -4,7 +4,8 @@
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title><?php echo Framework::getSystemName(); ?></title>
         <link rel="stylesheet" type="text/css" href="<?php echo $base; ?>webroot/css/login1.css" />
-        <link rel="stylesheet" type="text/css" href="<?php echo $base; ?>webroot/css/style1.css" />
+        <?php /*
+        <link rel="stylesheet" type="text/css" href="<?php echo $base; ?>webroot/css/style1.css" />*/ ?>
         <link rel="stylesheet" type="text/css" href="<?php echo $base; ?>webroot/css/meican3-theme/jquery-ui-1.8.16.custom.css" />
         <script language="JavaScript1.2" src="<?php echo $this->url(''); ?>webroot/js/jquery.min.js"></script>
         <script type="text/javascript" src="<?php echo $base; ?>webroot/js/jquery-ui-1.8.16.custom.min.js"></script>
@@ -44,7 +45,7 @@
 
 
             <div id="login_form" class="tab_content">
-                <h1 style="padding: 6px 0;"><?php echo _('Sign in to MEICAN'); ?></h1>
+                <h2 style="padding: 6px 0;"><?php echo _('Sign in to MEICAN'); ?></h2>
                 <hr>
                 <form name="login_form" method="POST" action="<?php echo $this->buildLink(array('action' => 'doLogin')); ?>">
                     <table>
