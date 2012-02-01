054b208930de59174d42ac7f0b354d0d5129f25f
Luís Armando Bianchin <labianchin@inf.ufrgs.br>
2012-02-01 18:12:18 +0000

    Removing target of menu links.
    git-svn-id: https://svn-redes.inf.ufrgs.br/hyman/trunks/meican@1070 f8bb852b-7d42-42d5-be0b-027218e41f69

--- apps/init/views/elements/menu.php
+++ apps/init/views/elements/menu.php
@@ -58,7 +58,7 @@
 	        <li>
                 <h3>
                     <?php if (!empty($menu->url)): ?>
-                        <a href="<?php echo $this->url($menu->url); ?>" target="main" class="top"><?php echo $menu->label; ?></a>
+                        <a href="<?php echo $this->url($menu->url); ?>" class="top"><?php echo $menu->label; ?></a>
                     <?php else: ?>
                         <a href="" class="top">
                     <span class="ui-icon ui-icon-circle-arrow-e"></span><?php echo $menu->label; ?></a>
@@ -67,7 +67,7 @@
             <?php if (!empty($menu->sub)): ?>
                 <ul>
                     <?php foreach ($menu->sub as $subMenu): ?>
-                        <li><a href="<?php echo $this->url($subMenu->url); ?>" target="main"><?php echo $subMenu->label; ?></a></li>
+                        <li><a href="<?php echo $this->url($subMenu->url); ?>"><?php echo $subMenu->label; ?></a></li>
                     <?php endforeach; ?>
                 </ul>
             <?php endif; ?>
