e31be9433c43de55a74eb4b48cc761677150dc74
Luís Armando Bianchin <labianchin@inf.ufrgs.br>
2012-02-08 18:37:54 +0000

    Fix for pjax on login screen.
    git-svn-id: https://svn-redes.inf.ufrgs.br/hyman/trunks/meican@1089 f8bb852b-7d42-42d5-be0b-027218e41f69

--- webroot/js/main.js
+++ webroot/js/main.js
@@ -151,33 +151,35 @@ function clearFlash(){
         $(this).find('div.subItem').addClass('ui-widget-content');*/
     };
     
-    $.extend($.pjax.defaults, {
-        error: function(jqXHR, textStatus) {
-            switch (jqXHR.status) {
-                case 401:
-                    top.location.href = baseUrl;
-                    break;
-                case 402:
-                    top.location.href = baseUrl;
-                    break;
-                case 404:
-                    setFlash("Page not found", 'error');
-                    break;
-                case 405:
-                    //change lang
-                    top.location.href = baseUrl+'init/gui';
-                    break;
-                case 406:
-                    //force refresh
-                    location.href = baseUrl+'init/gui';
-                    break;
-                default:
-                    setFlash("Unexpected error "+jqXHR.status, 'error');
-            }
-            console.debug("Error on ajax: "+jqXHR.status+jqXHR.statusText);
-        },
-        timeout: null
-    });
+    if (jQuery.isFunction(jQuery.fn.pjax)){  
+        $.extend($.pjax.defaults, {
+            error: function(jqXHR, textStatus) {
+                switch (jqXHR.status) {
+                    case 401:
+                        top.location.href = baseUrl;
+                        break;
+                    case 402:
+                        top.location.href = baseUrl;
+                        break;
+                    case 404:
+                        setFlash("Page not found", 'error');
+                        break;
+                    case 405:
+                        //change lang
+                        top.location.href = baseUrl+'init/gui';
+                        break;
+                    case 406:
+                        //force refresh
+                        location.href = baseUrl+'init/gui';
+                        break;
+                    default:
+                        setFlash("Unexpected error "+jqXHR.status, 'error');
+                }
+                console.debug("Error on ajax: "+jqXHR.status+jqXHR.statusText);
+            },
+            timeout: null
+        });
+    }
     
     
     $.fn.extend({
@@ -362,4 +364,74 @@ function clearFlash(){
 })(jQuery);
 
 /*! http://mths.be/placeholder v1.8.7 by @mathias */
-(function(f,h,c){var a='placeholder' in h.createElement('input'),d='placeholder' in h.createElement('textarea'),i=c.fn,j;if(a&&d){j=i.placeholder=function(){return this};j.input=j.textarea=true}else{j=i.placeholder=function(){return this.filter((a?'textarea':':input')+'[placeholder]').not('.placeholder').bind('focus.placeholder',b).bind('blur.placeholder',e).trigger('blur.placeholder').end()};j.input=a;j.textarea=d;c(function(){c(h).delegate('form','submit.placeholder',function(){var k=c('.placeholder',this).each(b);setTimeout(function(){k.each(e)},10)})});c(f).bind('unload.placeholder',function(){c('.placeholder').val('')})}function g(l){var k={},m=/^jQuery\d+$/;c.each(l.attributes,function(o,n){if(n.specified&&!m.test(n.name)){k[n.name]=n.value}});return k}function b(){var k=c(this);if(k.val()===k.attr('placeholder')&&k.hasClass('placeholder')){if(k.data('placeholder-password')){k.hide().next().show().focus().attr('id',k.removeAttr('id').data('placeholder-id'))}else{k.val('').removeClass('placeholder')}}}function e(){var o,n=c(this),k=n,m=this.id;if(n.val()===''){if(n.is(':password')){if(!n.data('placeholder-textinput')){try{o=n.clone().attr({type:'text'})}catch(l){o=c('<input>').attr(c.extend(g(this),{type:'text'}))}o.removeAttr('name').data('placeholder-password',true).data('placeholder-id',m).bind('focus.placeholder',b);n.data('placeholder-textinput',o).data('placeholder-id',m).before(o)}n=n.removeAttr('id').hide().prev().attr('id',m).show()}n.addClass('placeholder').val(n.attr('placeholder'))}else{n.removeClass('placeholder')}}}(this,document,jQuery));
+(function(f,h,c){
+    var a='placeholder' in h.createElement('input'),d='placeholder' in h.createElement('textarea'),i=c.fn,j;
+    if(a&&d){
+        j=i.placeholder=function(){
+            return this
+            };
+            
+        j.input=j.textarea=true
+        }else{
+        j=i.placeholder=function(){
+            return this.filter((a?'textarea':':input')+'[placeholder]').not('.placeholder').bind('focus.placeholder',b).bind('blur.placeholder',e).trigger('blur.placeholder').end()
+            };
+            
+        j.input=a;
+        j.textarea=d;
+        c(function(){
+            c(h).delegate('form','submit.placeholder',function(){
+                var k=c('.placeholder',this).each(b);
+                setTimeout(function(){
+                    k.each(e)
+                    },10)
+                })
+            });
+        c(f).bind('unload.placeholder',function(){
+            c('.placeholder').val('')
+            })
+        }
+        function g(l){
+        var k={},m=/^jQuery\d+$/;
+        c.each(l.attributes,function(o,n){
+            if(n.specified&&!m.test(n.name)){
+                k[n.name]=n.value
+                }
+            });
+    return k
+    }
+    function b(){
+    var k=c(this);
+    if(k.val()===k.attr('placeholder')&&k.hasClass('placeholder')){
+        if(k.data('placeholder-password')){
+            k.hide().next().show().focus().attr('id',k.removeAttr('id').data('placeholder-id'))
+            }else{
+            k.val('').removeClass('placeholder')
+            }
+        }
+}
+function e(){
+    var o,n=c(this),k=n,m=this.id;
+    if(n.val()===''){
+        if(n.is(':password')){
+            if(!n.data('placeholder-textinput')){
+                try{
+                    o=n.clone().attr({
+                        type:'text'
+                    })
+                    }catch(l){
+                    o=c('<input>').attr(c.extend(g(this),{
+                        type:'text'
+                    }))
+                    }
+                    o.removeAttr('name').data('placeholder-password',true).data('placeholder-id',m).bind('focus.placeholder',b);
+                n.data('placeholder-textinput',o).data('placeholder-id',m).before(o)
+                }
+                n=n.removeAttr('id').hide().prev().attr('id',m).show()
+            }
+            n.addClass('placeholder').val(n.attr('placeholder'))
+        }else{
+        n.removeClass('placeholder')
+        }
+    }
+}(this,document,jQuery));
