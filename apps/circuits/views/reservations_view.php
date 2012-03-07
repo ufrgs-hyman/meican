cb521e0094194d3c76e29f60470ba225215e15e0
Luís Armando Bianchin <labianchin@inf.ufrgs.br>
2012-03-07 20:26:43 +0000

    Shadow for the map of reservation view.
    git-svn-id: https://svn-redes.inf.ufrgs.br/hyman/trunks/meican@1116 f8bb852b-7d42-42d5-be0b-027218e41f69

--- apps/circuits/views/reservations_view.php
+++ apps/circuits/views/reservations_view.php
@@ -1,5 +1,4 @@
 <?php
-
 $gris = $this->passedArgs->gris;
 $flow = $this->passedArgs->flow;
 $timer = $this->passedArgs->timer;
@@ -9,7 +8,6 @@ $request = $this->passedArgs->request;
 $bandwidth = $this->passedArgs->bandwidth;
 $refresh = $this->passedArgs->refresh;
 $usr_login = $this->passedArgs->usr_login;
-
 ?>
 
 <h1><?php echo _("Reservation details"); ?></h1>
@@ -17,7 +15,7 @@ $usr_login = $this->passedArgs->usr_login;
 <table class="withoutBorder" style="width: 100%">
     <tr>
         <td style="width: 48%; vertical-align: top">
-            <div id="res_mapCanvas" style="width:100%; height:365px;"></div>        
+            <div id="res_mapCanvas" class="tab_subcontent" style="border: solid #BBB 1px; box-shadow: 2px 2px 4px #888; border-image: initial;width:100%; height:365px;"></div>       
         </td>
         <td style="width: 48%; padding-left: 15px; vertical-align: top">
             <table style="min-width: 30%" class="withoutBorder">
@@ -29,37 +27,38 @@ $usr_login = $this->passedArgs->usr_login;
                     <th style="border-bottom:none; padding-right: 5px; text-align: left"><?php echo _("User"); ?>: <?php echo $usr_login; ?></th>
                 </tr>
             </table>
-            
+
             <br/>
-            <?php $this->addElement('view_flow', $flow); ?>
-            
+<?php $this->addElement('view_flow', $flow); ?>
+
             <br/>
-            <?php $this->addElement('view_bandwidth', $bandwidth); ?>
-            
+<?php $this->addElement('view_bandwidth', $bandwidth); ?>
+
             <br/><br/>
-            <?php $this->addElement('view_timer', $timer); ?>
+<?php $this->addElement('view_timer', $timer); ?>
             <br/>
-            
-            <?php if ($request) $this->addElement('view_request', $request); ?>
+
+<?php if ($request)
+    $this->addElement('view_request', $request); ?>
 
             <?php if ($gris): ?>
-            
-            <form method="POST" action="<?php echo $this->buildLink(array('action' => 'cancel', 'param' => "res_id:$res_id,refresh:1")); ?>">    
-            
-                <?php if ($refresh): ?>
-                <div class="controls">
-                    <input type="button" class="refresh" value="<?php echo _("Refresh") ?>" onclick="griRefreshStatus(<?php echo $res_id; ?>);" />
-                    <input type="submit" class="cancel" disabled="disabled" id="cancel_button" value="<?php echo _("Cancel reservations"); ?>" onclick="return confirm('<?php echo _('Cancel the selected reservations?'); ?>')"/>
-                </div>
-                <br style="clear: both"/>
-                <?php endif; ?>
-            
-            <?php $this->addElement('list_gris', array('gris' => $gris, 'refresh' => $refresh)); ?>
-                
-            </form>
-            
-        <?php endif; ?>
-                
+
+                <form method="POST" action="<?php echo $this->buildLink(array('action' => 'cancel', 'param' => "res_id:$res_id,refresh:1")); ?>">    
+
+                    <?php if ($refresh): ?>
+                        <div class="controls">
+                            <input type="button" class="refresh" value="<?php echo _("Refresh") ?>" onclick="griRefreshStatus(<?php echo $res_id; ?>);" />
+                            <input type="submit" class="cancel" disabled="disabled" id="cancel_button" value="<?php echo _("Cancel reservations"); ?>" onclick="return confirm('<?php echo _('Cancel the selected reservations?'); ?>')"/>
+                        </div>
+                        <br style="clear: both"/>
+                    <?php endif; ?>
+
+                    <?php $this->addElement('list_gris', array('gris' => $gris, 'refresh' => $refresh)); ?>
+
+                </form>
+
+            <?php endif; ?>
+
         </td>
     </tr>
 </table>
@@ -67,5 +66,6 @@ $usr_login = $this->passedArgs->usr_login;
 <br/>
 
 <div class="controls">
-    <input class="back" type="button" onClick="redir('<?php $action = ($refresh) ? "status" : "history"; echo $this->buildLink(array("action" => $action)); ?>');" value="<?php echo _("Back to reservations"); ?>"/>
+    <input class="back" type="button" onClick="redir('<?php $action = ($refresh) ? "status" : "history";
+            echo $this->buildLink(array("action" => $action)); ?>');" value="<?php echo _("Back to reservations"); ?>"/>
 </div>
