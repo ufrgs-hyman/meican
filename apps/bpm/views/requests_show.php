b47dce83fe1668bc27a04941cd7883b554b68fc7
Luís Armando Bianchin <labianchin@inf.ufrgs.br>
2012-04-02 21:16:13 +0000

    Styles on request_show tables.
    git-svn-id: https://svn-redes.inf.ufrgs.br/hyman/trunks/meican@1227 f8bb852b-7d42-42d5-be0b-027218e41f69

--- apps/bpm/views/requests_show.php
+++ apps/bpm/views/requests_show.php
@@ -6,44 +6,46 @@ $finished = $args->finished;
 
 <form method="POST" action="<?php echo $this->buildLink(array('action' => 'delete')); ?>">
 
-<?php
-$arrayElem = array('app' => 'init', 'buttons' => array('delete'));
-echo $this->element('controls', $arrayElem);
-?>
-    
+    <?php
+    $arrayElem = array('app' => 'init', 'buttons' => array('delete'));
+    echo $this->element('controls', $arrayElem);
+    ?>
+
     <?php if ($pending): ?>
         <h3><?php echo _("Pending incoming requests"); ?></h3>
-        
+
         <table class="list">
-            <tr>
-                <th></th>
-                <th><?php echo _('Source domain') ?></th>
-                <th><?php echo _('Destination domain') ?></th>
-                <th><?php echo _('Requester') ?></th>
-                <th><?php echo _('Resource type') ?></th>
-                <th><?php echo _('Resource description') ?></th>
-                <th></th>
-            </tr>
-
-            <?php foreach ($pending as $p): ?>
+            <thead>
                 <tr>
-                    <?php //if ($f->deletable):  ?>
-                    <td>
-                        <input type="checkbox" name="del_checkbox[]" value="<?php echo $p->loc_id; ?>" />
-                    </td>
-                    <?php //endif;  ?>
-
-                    <td><?php echo $p->src_domain; ?></td>
-                    <td><?php echo $p->dst_domain; ?></td>
-                    <td><?php echo $p->src_user; ?></td>
-                    <td><?php echo $p->resc_type; ?></td>
-                    <td><?php echo $p->resc_descr; ?></td>
-                    <td class="edit">
-                        <input class="answer" type="button" value="<?php echo _('Answer'); ?>" onclick="redir('<?php echo $this->buildLink(array('action' => 'reply', 'param' => array('loc_id' => $p->loc_id))); ?> ')"/>
-                    </td>
+                    <th></th>
+                    <th><?php echo _('Source domain') ?></th>
+                    <th><?php echo _('Destination domain') ?></th>
+                    <th><?php echo _('Requester') ?></th>
+                    <th><?php echo _('Resource type') ?></th>
+                    <th><?php echo _('Resource description') ?></th>
+                    <th></th>
                 </tr>
-            <?php endforeach; ?>
-                
+            </thead>
+            <tbody>
+                <?php foreach ($pending as $p): ?>
+                    <tr>
+                        <?php //if ($f->deletable):  ?>
+                        <td>
+                            <input type="checkbox" name="del_checkbox[]" value="<?php echo $p->loc_id; ?>" />
+                        </td>
+                        <?php //endif;  ?>
+
+                        <td><?php echo $p->src_domain; ?></td>
+                        <td><?php echo $p->dst_domain; ?></td>
+                        <td><?php echo $p->src_user; ?></td>
+                        <td><?php echo $p->resc_type; ?></td>
+                        <td><?php echo $p->resc_descr; ?></td>
+                        <td class="edit">
+                            <input class="answer" type="button" value="<?php echo _('Answer'); ?>" onclick="redir('<?php echo $this->buildLink(array('action' => 'reply', 'param' => array('loc_id' => $p->loc_id))); ?> ')"/>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
         </table>
     <?php endif; ?>
 
@@ -51,34 +53,36 @@ echo $this->element('controls', $arrayElem);
         <h3><?php echo _("Finished incoming requests"); ?></h3>
 
         <table class="list">
-            <tr>
-                <th></th>
-                <th><?php echo _('Source domain') ?></th>
-                <th><?php echo _('Destination domain') ?></th>
-                <th><?php echo _('Requester') ?></th>
-                <th><?php echo _('Resource type') ?></th>
-                <th><?php echo _('Resource description') ?></th>
-                <th><?php echo _('Response') ?></th>
-                <th><?php echo _('Message') ?></th>
-            </tr>
-            
-            <?php foreach ($finished as $f): ?>
+            <thead>
                 <tr>
-                    <td>
-                        <?php //if ($f->deletable): ?>
-                        <input type="checkbox" name="del_checkbox[]" value="<?php echo $f->loc_id; ?>" />
-                        <?php //endif;  ?>
-                    </td>
-                    <td><?php echo $f->src_domain; ?></td>
-                    <td><?php echo $f->dst_domain; ?></td>
-                    <td><?php echo $f->src_user; ?></td>
-                    <td><?php echo $f->resc_type; ?></td>
-                    <td><?php echo $f->resc_descr; ?></td>
-                    <td><?php echo $f->response; ?></td>
-                    <td><?php echo $f->message; ?></td>
+                    <th></th>
+                    <th><?php echo _('Source domain') ?></th>
+                    <th><?php echo _('Destination domain') ?></th>
+                    <th><?php echo _('Requester') ?></th>
+                    <th><?php echo _('Resource type') ?></th>
+                    <th><?php echo _('Resource description') ?></th>
+                    <th><?php echo _('Response') ?></th>
+                    <th><?php echo _('Message') ?></th>
                 </tr>
-            <?php endforeach; ?>
-                
+            </thead>
+            <tbody>
+                <?php foreach ($finished as $f): ?>
+                    <tr>
+                        <td>
+                            <?php //if ($f->deletable): ?>
+                            <input type="checkbox" name="del_checkbox[]" value="<?php echo $f->loc_id; ?>" />
+                            <?php //endif;  ?>
+                        </td>
+                        <td><?php echo $f->src_domain; ?></td>
+                        <td><?php echo $f->dst_domain; ?></td>
+                        <td><?php echo $f->src_user; ?></td>
+                        <td><?php echo $f->resc_type; ?></td>
+                        <td><?php echo $f->resc_descr; ?></td>
+                        <td><?php echo $f->response; ?></td>
+                        <td><?php echo $f->message; ?></td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
         </table>
     <?php endif; ?>
 
