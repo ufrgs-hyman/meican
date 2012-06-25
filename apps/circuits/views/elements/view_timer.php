328bc79093f7d68c41a824a57ac9e7ad8850c5ef
Luís Armando Bianchin <labianchin@inf.ufrgs.br>
2012-06-25 19:05:57 +0000

    View Timer.
    git-svn-id: https://svn-redes.inf.ufrgs.br/hyman/trunks/meican@1341 f8bb852b-7d42-42d5-be0b-027218e41f69

--- apps/circuits/views/elements/view_timer.php
+++ apps/circuits/views/elements/view_timer.php
@@ -1,17 +1,15 @@
-<dl>
-    <dt><?= _("Start") ?></dt>
-    <dd><label id="view_startTimer"><?php if ($timer) echo $timer->start; ?></label></dd>
-    <dt><?= _("Finish") ?></dt>
-    <dd><label id="view_finishTimer"><?php if ($timer) echo $timer->finish; ?></label></dd>
-    <dt><?= _("Duration") ?></dt>
-    <dd><label id="view_durationTimer"><?php if ($timer) echo $timer->duration; ?></label></dd>
-    
-    <?php if (!$timer || $timer->summary): ?>
-            <dt>
-                <?= _("Summary") ?>
-            </dt>
-            <dd>
-                <label id="confirmation_summary"><?= !empty($timer)&&$timer->summary?$timer->summary:null ; ?></label>
-            </dd>
-    <?php endif; ?>
-</dl>
+<div>
+<div class="label-description"><?php echo _("Start") ?>: </div>
+<label><?php if ($timer) echo $timer->start; ?></label>
+<div class="label-description"><?php echo _("Finish") ?>: </div>
+<label><?php if ($timer) echo $timer->finish; ?></label>
+<div class="label-description"><?php echo _("Duration"); ?>: </div>
+<label><?php if ($timer) echo $timer->duration; ?></label>
+
+</div> 
+<?php if (!$timer || $timer->summary): ?>
+    <div>
+        <div class="label-description"><<?php echo _("Summary"); ?></div>
+        <label><?= !empty($timer) && $timer->summary ? $timer->summary : null; ?></label>
+    </div>
+<?php endif; ?>
