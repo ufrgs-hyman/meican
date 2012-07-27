259bcf289e6428b76799c74f08f099237c76b7b7
Felipe Nesello <fanesello@inf.ufrgs.br>
2012-07-27 20:29:17 +0000

    Updating install.sh script.
    git-svn-id: https://svn-redes.inf.ufrgs.br/hyman/trunks/meican@1381 f8bb852b-7d42-42d5-be0b-027218e41f69

--- db/install.sh
+++ db/install.sh
@@ -5,11 +5,9 @@ apt-get install -y apache2 mysql-server mysql-client php5 php5-mysql php-apc php
 # Configurando phpmyadmin: 
 #	- Selecionar servidor web: apache2
 #	- Configurar base com dbconfig-common? sim
-#	- Palavra-passe do administrador: 
-#	- Palavra-passe da aplicação Mysql: definida anteriormente na configuração do mysql
+#	- Palavra-passe do administrador: definida anteriormente na configuração do mysql
+#	- Palavra-passe da aplicação Mysql
 
-pear install mdb2
-pear install pear/MDB2#mysql
 pear install Mail
 pear install Net_SMTP
 a2enmod rewrite #libera mod_rewrite no apache
@@ -17,12 +15,11 @@ a2enmod rewrite #libera mod_rewrite no apache
 #mkdir /home/www
 #chmod 755 /home/www/
 cd /var/www #download do svn
-svn checkout https://svn-redes.inf.ufrgs.br/hyman/branches/meican2706/ meican-2706
-svn checkout https://svn-redes.inf.ufrgs.br/hyman/trunks/meican meican-main
+svn checkout https://svn-redes.inf.ufrgs.br/hyman/tags/meican_v2-2 meican-main
+#svn checkout https://svn-redes.inf.ufrgs.br/hyman/trunks/meican meican-trunk
 ln -ns /var/www/meican-main/ /var/www/meican
 
 chown -R www-data:www-data /var/www/meican-main/log
-chown -R www-data:www-data /var/www/meican-2706/log
 
 cp /var/www/meican-main/db/meican.conf /etc/apache2/conf.d/ #Colocar configuração do meican no apache
 
@@ -31,9 +28,9 @@ nano /etc/apache2/sites-available/default #colocar /var/www/meican como document
 
 /etc/init.d/apache2 restart
 
-
 nano /var/www/meican-main/db/build.sh #configurar usuário e senha do mysql no script de importação do banco
 
-bash /var/www/meican-main/db/build.sh
+cd /var/www/meican-main/db/
+bash build.sh
 
-nano /var/www/meican-main/meican.conf.php #editar arquivo de configuração do meican
+nano /var/www/meican-main/config/local.php #editar arquivo de configuração do meican
