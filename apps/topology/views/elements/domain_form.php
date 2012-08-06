3b278fb970af54ba4e593e1d0f0c1927e1f58b18
Luís Armando Bianchin <labianchin@inf.ufrgs.br>
2012-08-06 19:42:37 +0000

    Domain Info form.
    git-svn-id: https://svn-redes.inf.ufrgs.br/hyman/trunks/meican@1398 f8bb852b-7d42-42d5-be0b-027218e41f69

--- apps/topology/views/elements/domain_form.php
+++ apps/topology/views/elements/domain_form.php
@@ -15,93 +15,49 @@ $dom_version = isset($argsToElement->dom_version) ? $argsToElement->dom_version 
 
 ?>
 
-<table class="add">
-    <tr>
-        <th class="right">
-            <?php echo _("Name"); ?>:
-        </th>
-        <td class="left">
-            <input type="text" name="dom_descr" size="30" value="<?php echo $dom_descr; ?>"/>
-        </td>
-    </tr>
-    <tr>
-        <th class="right">
-            <?php echo _("OSCARS IP"); ?>:
-        </th>
-        <td class="left">
-            <input type="text" name="oscars_ip" id="oscars_ip" size="30" value="<?php echo $oscars_ip; ?>" onkeyup="buildIDC_URL()"/>
-        </td>
-    </tr>
-    <tr>
-        <th class="right">
-            <?php echo _("Protocol");?>:
-        </th>
-        <td class="left">
-            <input type="radio" name="oscars_protocol" id="http" value="http" <?php if ($oscars_protocol == 'http') echo checked; ?> onchange="buildIDC_URL()"/> HTTP &nbsp;&nbsp;
-            <input type="radio" name="oscars_protocol" id="https" value="https" <?php if ($oscars_protocol == 'https') echo checked; ?> onchange="buildIDC_URL()"/> HTTPS
-        </td>
-    </tr>
-    <tr>
-        <th class="right">
-            <?php echo _("IDC URL"); ?>:
-        </th>
-        <td class="left">
-            <label id="idc_url"><?php echo $idc_url; ?></label>
-            <input type="text" name="idc_url" id="input_idcUrl" hidden="true" value="<?php echo $idc_url; ?>"/>
-        </td>
-    </tr>
-    <tr>
-        <th class="right">
-            <?php echo _("OSCARS Version"); ?>:
-        </th>
-        <td class="left">
-            <select name="dom_version" size="1" style="width:222px">
-                <option <?php if ($dom_version == '0.5.3') echo 'selected="true"'; ?> value="0.5.3"> OSCARS 0.5.3 </option>
-                <option <?php if ($dom_version == '0.5.4') echo 'selected="true"'; ?> value="0.5.4"> OSCARS 0.5.4 </option>
-                <option <?php if ($dom_version == '0.6') echo 'selected="true"'; ?> value="0.6">   OSCARS 0.6   </option>
-            </select>
-        </td>
-    </tr>
-    <tr>
-        <th class="right">
-            <?php echo _("Topology ID"); ?>:
-        </th>
-        <td class="left">
-            <input type="text" name="topology_id" size="30" value="<?php echo $topology_id; ?>"/>
-        </td>
-    </tr>
-    
-    <tr>
-        <th class="right">
-            <?php echo _("ODE IP"); ?>:
-        </th>
-        <td class="left">
-            <input type="text" name="ode_ip" size="30" value="<?php echo $ode_ip; ?>"/>
-        </td>
-    </tr>
-    <tr>
-        <th class="right">
-            <?php echo _("ODE WSDL path"); ?>:
-        </th>
-        <td class="left">
-            <input type="text" name="ode_wsdl_path" size="30" value="<?php echo $ode_wsdl_path; ?>"/>
-        </td>
-    </tr>
-    <tr>
-        <th class="right">
-            <?php echo _("ODE start function"); ?>:
-        </th>
-        <td class="left">
-            <input type="text" name="ode_start" size="30" value="<?php echo $ode_start; ?>"/>
-        </td>
-    </tr>
-    <tr>
-        <th class="right">
-            <?php echo _("ODE response function"); ?>:
-        </th>
-        <td class="left">
-            <input type="text" name="ode_response" size="30" value="<?php echo $ode_response; ?>"/>
-        </td>
-    </tr>
-    
-</table>
+<div class="form input">
+    <label for="dom_descr"><?php echo _("Name"); ?></label>
+    <input type="text" name="dom_descr" size="30" value="<?php echo $dom_descr; ?>"/>
+</div>
+<div class="form input">
+    <label for="oscars_ip"><?php echo _("OSCARS IP"); ?></label>
+    <input type="text" name="oscars_ip" size="30" id="oscars_ip" value="<?php echo $oscars_ip; ?>" onkeyup="buildIDC_URL()"/>
+</div>
+<div class="form input">
+    <label for="oscars_protocol"><?php echo _("Protocol"); ?></label>
+    <input type="radio" name="oscars_protocol" id="http" value="http" <?php if ($oscars_protocol == 'http') echo checked; ?> onchange="buildIDC_URL()"/> HTTP &nbsp;&nbsp;
+    <input type="radio" name="oscars_protocol" id="https" value="https" <?php if ($oscars_protocol == 'https') echo checked; ?> onchange="buildIDC_URL()"/> HTTPS
+</div>
+<div class="form input">
+    <label for="idc_url"><?php echo _("IDC URL"); ?></label>
+    <label id="idc_url"><?php echo $idc_url; ?></label>
+    <input type="text" name="idc_url" id="input_idcUrl" hidden="true" value="<?php echo $idc_url; ?>"/>
+</div>
+<div class="form input">
+    <label for="dom_version"><?php echo _("OSCARS Version"); ?></label>
+    <select name="dom_version" size="1" style="width:222px">
+        <option <?php if ($dom_version == '0.5.3') echo 'selected="true"'; ?> value="0.5.3"> OSCARS 0.5.3 </option>
+        <option <?php if ($dom_version == '0.5.4') echo 'selected="true"'; ?> value="0.5.4"> OSCARS 0.5.4 </option>
+        <option <?php if ($dom_version == '0.6') echo 'selected="true"'; ?> value="0.6">   OSCARS 0.6   </option>
+    </select>
+</div>
+<div class="form input">
+    <label for="topology_id"><?php echo _("Topology ID"); ?></label>
+    <input type="text" name="topology_id" size="30" value="<?php echo $topology_id; ?>"/>
+</div>
+<div class="form input">
+    <label for="ode_ip"><?php echo _("ODE IP"); ?></label>
+    <input type="text" name="ode_ip" size="30" value="<?php echo $ode_ip; ?>"/>
+</div>
+<div class="form input">
+    <label for="ode_wsdl_path"><?php echo _("ODE WSDL path"); ?></label>
+    <input type="text" name="ode_wsdl_path" size="30" value="<?php echo $ode_wsdl_path; ?>"/>
+</div>
+<div class="form input">
+    <label for="ode_start"><?php echo _("ODE start function"); ?></label>
+    <input type="text" name="ode_start" size="30" value="<?php echo $ode_start; ?>"/>
+</div>
+<div class="form input">
+    <label for="ode_response"><?php echo _("ODE response function"); ?></label>
+    <input type="text" name="ode_response" size="30" value="<?php echo $ode_response; ?>"/>
+</div>
