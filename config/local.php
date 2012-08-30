8c258fb1d9c2f2c2fb766bd256df56ab505593bd
Jeremy <jplante@es.net>
2012-08-30 11:08:22 -0700

    Restore some files
    I guess I put these into .gitignore at some point. They are needed for
    MEICAN to work.

---
+++ config/local.php
@@ -0,0 +1,19 @@
+<?php
+return array(
+    'defaultLang' => 'en_US.utf8',
+    'databases' => array(
+        'default' => array(
+            'datasource' => 'Database/Mysql',
+            'persistent' => false,
+            'host' => 'localhost',
+            'login' => 'root',
+            'password' => 'root',
+            'database' => 'meican',
+            'prefix' => '',
+        //'encoding' => 'utf8',
+        )
+    ),
+    'useDatabase' => 'default',
+    'debug' => 1,
+	'MapsAPIKey' => 'AIzaSyBKXURpWAsB4VEKgrPUx8a6w2vZ7cwd_8U'
+);
