fee1192bfb045b793a15ea26a62af744239534fe
Felipe Nesello <fanesello@inf.ufrgs.br>
2012-10-11 14:46:46 +0000

    Making some modifications on login's Portuguese text.
    git-svn-id: https://svn-redes.inf.ufrgs.br/hyman/trunks/meican@1442 f8bb852b-7d42-42d5-be0b-027218e41f69

--- apps/init/views/elements/text_info_pt.php
+++ apps/init/views/elements/text_info_pt.php
@@ -3,26 +3,26 @@
 <div class="center"><img src="<?= $base ?>webroot/img/meican_new.png" class="logo" alt="MEICAN"/></div>
 <h2><?php echo Configure::read('systemName'); ?></h2>
 
-<p>MEICAN permite que usuários de redes requisitem , de maneira amigável, circuitos dedicados em
-    <a href="http://en.wikipedia.org/wiki/Dynamic_circuit_network">Redes de Circuitos Dinâmicas</a>. 
-    MEICAN também permite operadores de rede avaliar e aceitar requisições de circuitos de outros usuários em ambientes com 
+<p>O MEICAN permite que usuários de redes requisitem, de uma maneira amigável, circuitos dedicados em
+    <a href="http://en.wikipedia.org/wiki/Dynamic_circuit_network">Redes de Circuitos Dinâmicos</a>. 
+    O MEICAN também permite que operadores de rede avaliem e aceitem requisições de circuitos de outros usuários em ambientes com 
     multiplos domínios.
-    Com MEICAN, você pode:</p>
+    Com o MEICAN, você pode:</p>
 
 <ul>
     <li>
         <img src="<?= $base ?>webroot/img/bullet1.jpg" alt="Bullet" class="bullet"/>
         <b>Requisitar Circuitos</b>
-        <p>Circuitos de usuários finais podem ser agendados para serem criados e desativados quando for mais conveniente.</p>
+        <p>Circuitos de usuários finais podem ser agendados para serem configurados e removidos quando for mais conveniente.</p>
     </li>
     <li>
         <img src="<?= $base ?>webroot/img/bullet2.jpg" alt="Bullet" class="bullet"/>
-        <b>Autorisar Requisições</b>
+        <b>Autorizar Requisições</b>
         <p>Operadores de rede podem ser notificados para aceitar ou rejeitar requisições de estabelecimento de novos circuitos.</p>
     </li>
     <li>
         <img src="<?= $base ?>webroot/img/bullet3.jpg" alt="Bullet" class="bullet"/>
-        <b>Costrução de Políticas Automatizadas</b>
-        <p>Workflows de autorização podem ser usados para automatizar o processo de decisão entre múltiplos domínios onde passam circuitos de usuários finais.</p>
+        <b>Costruir Políticas Automatizadas</b>
+        <p>Workflows de autorização podem ser utilizados para automatizar o processo de tomada de decisão entre os múltiplos domínios por onde passam os circuitos de usuários finais.</p>
     </li>
 </ul>
