a1420c68dcb417150f43699ddf40782a3baa50d4
mqgmaster <mqgmaster@gmail.com>
2015-06-17 14:32:18 -0300

    redefined global params

---
+++ config/params.php
@@ -0,0 +1,24 @@
+<?php
+
+return [
+	"meican.version" 					=> "2.1.0.310",
+		
+	"google.analytics.enabled" 			=> false,
+	"google.analytics.key" 				=> "",
+		
+    'aggregator.importer.enabled'		=> false,
+	'aggregator.default.url'			=> "",
+			
+	'meican.certificate.filename'		=> "",
+	'meican.certificate.passphrase'		=> "",	
+		
+	'meican.nsa.id'						=> "",
+	
+	'meican.connection.requester.url' 	=> '',	
+	
+	"perfsonar.importer.enabled"		=> false,
+	"perfsonar.default.url"				=> "",
+	
+	"provider.force.dummy" 				=> false,
+
+];
