cea2eb94fb8590022c74f6f6acc98463e79f14ad
Maurício Quatrin Guerreiro <mqgmaster@users.noreply.github.com>
2015-06-30 00:35:14 -0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -14,10 +14,7 @@ sudo ln -s /usr/share/phpmyadmin /var/www/phpmyadmin
 echo "Creating database. Please, type the password of the MySQL root user:"
 mysqladmin -u root -p create meican2
 sudo a2enmod rewrite
-cd ~/
-git clone https://github.com/ufrgs-hyman/meican2.git
 read -n1 -r -p "Please, in the next step confirm the database settings for MEICAN. Press any key to continue." key
-cd meican2
 nano config/db.php
 curl -sS https://getcomposer.org/installer | php
 php composer.phar global require "fxp/composer-asset-plugin:1.0.0"
