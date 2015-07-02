5347b1bfc307eab99ed1d9fa8a9db76618a8338d
DiegoPittol <diegokindin@gmail.com>
2015-07-02 14:31:58 -0300

    Fix: Mudancas na tabela Notification

---
+++ migrations/m150626_171138_diego.php
@@ -0,0 +1,45 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150626_171138_diego extends Migration
+{
+    public function up()
+    {
+    	$this->execute("
+DELETE FROM `meican_notification`;
+		");
+    	 
+    	$this->execute("
+ALTER TABLE `meican_notification` CHANGE `type` `type` ENUM('AUTHORIZATION','TOPOLOGY','RESERVATION','NOTICE') CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL;
+		");
+    	
+    	$this->execute("
+ALTER TABLE `meican_notification` ADD INDEX(`type`);
+    	");
+    	
+    	$this->execute("
+ALTER TABLE `meican_notification` ADD INDEX(`user_id`);
+    	");
+    	
+    }
+
+    public function down()
+    {
+        echo "m150626_171138_diego cannot be reverted.\n";
+
+        return false;
+    }
+    
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+    
+    public function safeDown()
+    {
+    }
+    */
+}
