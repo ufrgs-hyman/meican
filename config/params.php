e8443b0d08bb1d53e39e1f4637b2e152000339c5
mqgmaster <mqgmaster@gmail.com>
2015-09-28 17:14:47 -0300

    fix: mailer params

--- config/params.php
+++ config/params.php
@@ -1,17 +1,19 @@
 <?php
 
 return [
-	"meican.version" => "2.2.0",
-		
-	"google.analytics.enabled" => false,
-	"google.analytics.key" => "",
+    "meican.version" => "2.2.0",
+
+    "google.analytics.enabled" => false,
+    "google.analytics.key" => "",
     "google.maps.key" => false,
     "google.recaptcha.secret.key" => "",
     "google.recaptcha.site.key" => "",
 
-	'meican.certificate.filename' => "",
-	'meican.certificate.passphrase' => "",	
-		
-	"provider.force.dummy" => true,
+    'mailer.destination' => 'meican@inf.ufrgs.br',
+
+    'meican.certificate.filename' => "",
+    'meican.certificate.passphrase' => "",	
+
+    "provider.force.dummy" => true,
 
 ];
