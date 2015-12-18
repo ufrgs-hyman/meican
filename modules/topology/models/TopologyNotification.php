97c137a0a865b117a08f7565ba79068d9c25cddc
mqgmaster <mqgmaster@gmail.com>
2015-12-18 15:50:55 -0200

    fix: topology notification

--- modules/topology/models/TopologyNotification.php
+++ modules/topology/models/TopologyNotification.php
@@ -41,10 +41,10 @@ class TopologyNotification {
         $title = Yii::t("notification", 'Topology change');
         if (count($changes) > 1) {
             $msg = Yii::t("notification", 'The topologies of')." <b>".count($changes)."</b> ".Yii::t("notification", 'domains has been updated.')." <b>".
-            $appliedChangesSize."</b> ".Yii::t("notification", 'changes were applied.').' '.($pendingChangesSize > 0 ? " <b>".$pendingChangesSize."</b> ".Yii::t("notification", 'are pending.') : '.');
+            $appliedChangesSize."</b> ".Yii::t("notification", 'changes were applied.').' '.($pendingChangesSize > 0 ? " <b>".$pendingChangesSize."</b> ".Yii::t("notification", 'are pending.') : '');
         } else if (count($changes) == 1){
             $msg = Yii::t("notification", 'The')." <b>".$changes[0]['domain']."</b> ".Yii::t("notification", 'topology has been updated.')." <b>".
-            $appliedChangesSize."</b> ".Yii::t("notification", 'changes were applied.').' '.($pendingChangesSize > 0 ? " <b>".$pendingChangesSize."</b> ".Yii::t("notification", 'are pending.') : '.');
+            $appliedChangesSize."</b> ".Yii::t("notification", 'changes were applied.').' '.($pendingChangesSize > 0 ? " <b>".$pendingChangesSize."</b> ".Yii::t("notification", 'are pending.') : '');
         } else return "";
         
         $date = Yii::$app->formatter->asDatetime($notification->date);
