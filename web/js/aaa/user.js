b9efef71ac647de53d921ead8d6c395e9e3a23d1
mqgmaster <mqgmaster@gmail.com>
2015-12-25 21:40:40 -0200

    fix: libs must be managed via composer

--- web/js/aaa/user.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('#accountform-ischangedpass').click(function() {
-    	$('#changePasswordForm').slideToggle();
-    });
-});
-
-function submitDeleteForm() {
-	$("#user-form").submit();
-}
