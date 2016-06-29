331a18710d1abd611cb715b8124e91b9e6465ac1
mqgmaster <mqgmaster@gmail.com>
2016-06-28 22:21:38 -0300

    fix: config

--- config/web.php
+++ config/web.php
@@ -64,7 +64,7 @@ $config = [
         ],
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => 'sadasddsad',
+            'cookieValidationKey' => '',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
