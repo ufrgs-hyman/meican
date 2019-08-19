a3b80d85bc4c23cb6cebfa14dff2d3fd04068292
Leonardo Santos <leonardolauryel@hotmail.com>
2019-08-19 12:46:57 -0300

    Version change to 3.1.2

--- config/web.php
+++ config/web.php
@@ -9,7 +9,7 @@ $params = require(__DIR__ . '/params.php');
 $config = [
     'id' => 'meican',
     'name'=>'MEICAN - Management Environment of Inter-domain Circuits for Advanced Networks',
-    'version' => '3.1.1',
+    'version' => '3.1.2',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log','session',
         'notify',
