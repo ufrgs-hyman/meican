f299bba2baf6dcddf258a86757baa5034e57bb9d
Rafael Hengen Ribeiro <rafaelhr.ribeiro@gmail.com>
2019-10-16 08:56:29 -0300

    Testing travis

--- config/db.php
+++ config/db.php
@@ -6,9 +6,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=meican31',
+    'dsn' => 'mysql:host=localhost;dbname=meican2',
     'username' => 'root',
-    'password' => 'root',
+    'password' => '',
     'charset' => 'utf8',
     'tablePrefix' => 'meican_',
 ];
