8be412ddf62f82fe0822ba03fa7f7ca0cc3eb624
Rafael Hengen Ribeiro <rafaelhr.ribeiro@gmail.com>
2019-12-10 16:14:56 -0300

    Update filtering by circuit status

--- modules/circuits/views/reservation/status/_grid.php
+++ modules/circuits/views/reservation/status/_grid.php
@@ -129,15 +129,11 @@ function generateGrid($gridId, $data, $searchModel, $allowedDomains){
 
                         return $msg;
                     },
-                    // 'filter' => Html::activeDropDownList($searchModel, 'dataplane_status', 
-                    //     ArrayHelper::map([
-                    //         ['id' => 'ACTIVE', 'name' => 'Active'],
-                    //         ['id' => 'INACTIVE', 'name' => 'Inactive']
-                    //     ], 'id', 'name'),
-                    //     ['id'=>'reservationsearch-dataplane_status', 'class'=>'form-control','prompt' => Yii::t("circuits", 'any')]
-                    // ),
-                    'filter' => Html::activeCheckbox($searchModel, 'dataplane_status', 
-                        ['id'=>'reservationsearch-dataplane_status', 'name' => 'Active']
+                    'filter' => Html::activeDropDownList($searchModel, 'dataplane_status', 
+                        ArrayHelper::map([
+                            ['id' => 'ACTIVE', 'name' => 'Active'],
+                        ], 'id', 'name'),
+                        ['id'=>'reservationsearch-dataplane_status', 'class'=>'form-control','prompt' => Yii::t("circuits", 'any')]
                     ),
                     'headerOptions'=>['style'=>'width: 28%;'],
                 ],
