9c105b458225a36b09a68d1e77ce8b3af4450756
Rafael Hengen Ribeiro <rafaelhr.ribeiro@gmail.com>
2019-12-11 14:11:47 -0300

    Improving performance

--- modules/circuits/views/reservation/status/_grid.php
+++ modules/circuits/views/reservation/status/_grid.php
@@ -116,17 +116,13 @@ function generateGrid($gridId, $data, $searchModel, $allowedDomains){
                         $status = $model->getStatus();
                         $dataStatus = $model->getDataStatus();
 
-                        $custom_status = [
-                            'Terminated' => $status == 'Cancelled' and $dataStatus == 'Active',
-                            'Terminating' => $status == 'Cancelling' and $dataStatus == 'Active'
-                        ];
                         $msg = '';
-                        foreach($custom_status as $name => $condition)
-                            if($condition)
-                                $msg = $name;
-
-                        if(empty($msg))
-                            $msg = $status .", ".$model->getAuthStatus().", ". $dataStatus;
+                        if($dataStatus == 'Active' and $status == 'Cancelled')
+                            $msg = 'Terminated';
+                        else if($dataStatus == 'Active' and $status == 'Cancelling')
+                            $msg = 'Terminating';
+                        else
+                            $msg = $status . ", " . $model->getAuthStatus() . ", " . $dataStatus;
 
                         return $msg;
                     },
