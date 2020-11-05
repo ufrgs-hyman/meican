1e82b08a9005b7086a184b8787beef19f421510d
leonardolauryel <llbsantos@inf.ufrgs.br>
2020-11-05 07:34:35 -0300

    Sending container info by POST

--- modules/circuits/views/resources/create.php
+++ modules/circuits/views/resources/create.php
@@ -20,8 +20,21 @@ use yii\helpers\Url;
 <?php
 
 // URL on which we have to post data
-$url = "http://blacksabbath.inf.ufrgs.br:15443/aggregator/index.php/aggregator/request_container?container_name=$model->container_name&container_port=$model->container_port";
-$ch = curl_init();
+//$url = "http://blacksabbath.inf.ufrgs.br:15443/aggregator/index.php/aggregator/request_container?container_name=$model->container_name&container_port=$model->container_port";
+$url = "http://blacksabbath.inf.ufrgs.br:15443/aggregator/index.php/aggregator/request_container";
+/* $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $url);
+$response = curl_exec($ch); */
+
+
+$ch = curl_init( $url );
+# Setup request to send json via POST.
+$payload = json_encode( array( "container_name"=> $model->container_name, "container_port"=> $model->container_port ) );
+curl_setopt( $ch, CURLOPT_POSTFIELDS, $payload );
+curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+# Return response instead of printing.
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+# Send request.
 $response = curl_exec($ch);
+curl_close($ch);
 echo $response;
