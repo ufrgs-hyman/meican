affe40e5954a9bba37050e8974b8fa91d7491e1c
Eduardo Peretto <eduardo.peretto@ufrgs.br>
2021-05-27 09:54:28 -0300

    All permission issues were moved to branch 'permission_fixes'

--- modules/base/views/layouts/sidebar-menu.php
+++ modules/base/views/layouts/sidebar-menu.php
@@ -78,7 +78,6 @@ echo Menu::widget([
                     ['label'=>Yii::t('aaa','Groups'), 'url'=>['/aaa/group'], 'visible'=>RbacController::can('group/read')],
                     ['label'=>Yii::t('aaa','Configuration'), 'url'=>['/aaa/config'], 'visible'=>RbacController::can('group/update')],
             ],
-            'visible'=>(RbacController::can('user/read') || RbacController::can('role/read'))
         ],
     ],
 ]);
