ae0c5b3082ac61f05b87379825b4fdbae8e89263
leonardolauryel <llbsantos@inf.ufrgs.br>
2022-07-19 23:48:15 -0300

    Adding success message when container is ready

--- docker_for_build/docker-entrypoint.sh
+++ docker_for_build/docker-entrypoint.sh
@@ -5,7 +5,9 @@ cp $MEICAN_DIR/docker_for_build/db.php $MEICAN_DIR/config/ \
  && sed -i "s/MYSQL_DATABASE/$MYSQL_DATABASE/" $MEICAN_DIR/config/db.php \
  && sed -i "s/MYSQL_USER/$MYSQL_USER/" $MEICAN_DIR/config/db.php \
  && sed -i "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/" $MEICAN_DIR/config/db.php \
- && chown meican:meican $MEICAN_DIR/config/db.php  \
+ && chown meican:meican $MEICAN_DIR/config/db.php \
  && su meican -c "php composer.phar install" \
  && service apache2 start \
+ && echo "\033[0;32m MEICAN started successfully \033[0m " \
+ && echo "\033[0;32m Running on http://localhost:$MEICAN_PORT \033[0m " \
  && /bin/bash
